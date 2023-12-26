00a7f3967958b41b9040c479fa01cf346b0e60a5
Qui Nguyen <anhqui@gmail.com>
2023-12-26 18:46:53 -0500

    Update Navbar.vue

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -24,9 +24,10 @@ const router = useRouter();
 
 const auth = getAuth();
 
-let isLoggedIn = ref(true);
+let isLoggedIn = ref(false);
 let currentUser = ref(false);
 
+  isLoggedIn.value = true;
 
 const logout = () =>{
 signOut(auth).then(()=>{
