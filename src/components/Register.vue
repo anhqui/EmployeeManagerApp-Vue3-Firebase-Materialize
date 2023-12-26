b57928b501274bf11fe67ef4e90b40397d479dc1
Qui Nguyen <anhqui@gmail.com>
2023-12-26 18:01:01 -0500

    Update Register.vue

--- src/components/Register.vue
+++ src/components/Register.vue
@@ -37,8 +37,8 @@ const register = () => {
     createUserWithEmailAndPassword(auth, email.value, password.value)
     .then(userCredential=>{
         alert(`Account created for ${userCredential.user.email}`)
-        // router.push('/')
-          router.go({path: router.path})
+         router.push('/')
+         // router.go({path: router.path})
     })
     .catch(error=>{
         alert(error.message)
