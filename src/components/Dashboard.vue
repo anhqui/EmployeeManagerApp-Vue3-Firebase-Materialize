93f34d730f71fc0c9b886722cf7ac0654e0d8d1c
Qui Nguyen <anhqui@gmail.com>
2023-12-26 18:50:08 -0500

    Update Dashboard.vue
    update

--- src/components/Dashboard.vue
+++ src/components/Dashboard.vue
@@ -23,12 +23,8 @@ import {ref} from 'vue'
 import {onMounted, ref} from 'vue'
 import db from './firebaseInit'
 import { collection, getDocs, orderBy, query } from "firebase/firestore"; 
-   // import { getAuth } from "firebase/auth";
 
 const employees = ref([])
-// const isLoggedIn = ref(false);
-//     const currentUser = ref(false);
-//      const auth = getAuth();
 
 onMounted(async ()=>{
     // const querySnapshot = await getDocs(collection(db, "employees"));
@@ -48,18 +44,7 @@ employees.value.push(data)
    
 });
 
-//         if(auth.currentUser){
-// isLoggedIn.value = true;
-// currentUser.value = auth.currentUser.email;
-// }else{
-//   isLoggedIn.value = false;
-// }
- 
 })
-
-
-
-
 
 </script>
 
